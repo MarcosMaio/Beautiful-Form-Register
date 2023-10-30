f338817b6c36b22380ee2cc5eff3f41d2800efc6
Marcos Maio <120725100+MarcosMaio@users.noreply.github.com>
2023-10-30 00:56:51 -0300

    feat: add Login page to layout

--- src/app/Login/page.jsx
+++ src/app/Login/page.jsx
@@ -1,50 +1,11 @@
-import { faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import styles from "./styles/Login.module.css";
-import FormContainer from "../../components/Form-Container";
+import React from "react";
+import LoginForm from "./components/login-form";
+import MessageProviderWrapper from "@/providers/message-provider";
 
-const Login = () => {
+export default function Login() {
   return (
-    <FormContainer>
-      <div className={styles["form-box"]}>
-        <h2 className={styles["input-title"]}>Login</h2>
-        <form action="#">
-          <div className={styles["input-box"]}>
-            <span className={styles.icon}>
-              <FontAwesomeIcon icon={faEnvelope} />
-            </span>
-            <input type="email" name="email" required />
-            <label htmlFor="email">Email</label>
-          </div>
-          <div className={styles["input-box"]}>
-            <span className={styles.icon}>
-              <FontAwesomeIcon icon={faLock} />
-            </span>
-            <input type="password" name="password" required />
-            <label htmlFor="password">Password</label>
-          </div>
-          <div className={styles["remember-forgot"]}>
-            <label>
-              <input type="checkbox" />
-              Remember me
-            </label>
-            <a href="#">Forgot Password?</a>
-          </div>
-          <button type="submit" className={styles.btn}>
-            Login
-          </button>
-          <div className={styles["login-register"]}>
-            <p>
-              Don't have an account?
-              <a href="/Register" className={styles["register-link"]}>
-                Register
-              </a>
-            </p>
-          </div>
-        </form>
-      </div>
-    </FormContainer>
+    <MessageProviderWrapper>
+      <LoginForm />
+    </MessageProviderWrapper>
   );
-};
-
-export default Login;
+}
