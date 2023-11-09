b62f2e8bbbe581757c50501185d3f57dbed25912
Marcos Maio <120725100+MarcosMaio@users.noreply.github.com>
2023-11-08 21:31:26 -0300

    feat: add SendEmailVerification component to Login page and make a logic to switch forms when needed

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,11 +1,23 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import LoginForm from "./components/login-form";
 import MessageProviderWrapper from "@/providers/message-provider";
+import SendEmailVerification from "./components/send-email-verification";
 
 export default function Login() {
+  const [isForgotPassword, SetIsForgotPassword] =
+    useState<React.SetStateAction<boolean>>(false);
+
   return (
     <MessageProviderWrapper>
-      <LoginForm />
+      <>
+        {isForgotPassword ? (
+          <SendEmailVerification />
+        ) : (
+          <LoginForm SetIsForgotPassword={SetIsForgotPassword} />
+        )}
+      </>
     </MessageProviderWrapper>
   );
 }
