9aceaa127b9a0c7a2d8147d70325c60bba14d372
Marcos Maio <120725100+MarcosMaio@users.noreply.github.com>
2023-11-11 22:44:55 -0300

    refactor: change login page to contain only the login form

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -1,23 +1,7 @@
 "use client";
 
-import React, { useState } from "react";
 import LoginForm from "./components/login-form";
-import MessageProviderWrapper from "@/providers/message-provider";
-import SendEmailVerification from "./components/send-email-verification";
 
 export default function Login() {
-  const [isForgotPassword, SetIsForgotPassword] =
-    useState<React.SetStateAction<boolean>>(false);
-
-  return (
-    <MessageProviderWrapper>
-      <>
-        {isForgotPassword ? (
-          <SendEmailVerification />
-        ) : (
-          <LoginForm SetIsForgotPassword={SetIsForgotPassword} />
-        )}
-      </>
-    </MessageProviderWrapper>
-  );
+  return <LoginForm />;
 }
