d9b82e6cee90a247faf52b070b1c33cfbbd9c967
Marcos Maio <120725100+MarcosMaio@users.noreply.github.com>
2023-11-15 12:21:31 -0300

    refactor: delete the message provider from register page

--- src/app/register-user/page.tsx
+++ src/app/register-user/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import MessageProviderWrapper from "@/providers/message-provider";
 import RegisterForm from "./components/Register";
 export default function Register() {
   return (
