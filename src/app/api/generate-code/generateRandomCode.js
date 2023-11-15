fc47a0f2f6549419b8a98391c9476fca67d42aa6
Marcos Maio <120725100+MarcosMaio@users.noreply.github.com>
2023-11-15 10:57:55 -0300

    refactor: change the generator random code file to generate a 6 characters code

--- src/app/api/generate-code/generateRandomCode.js
+++ src/app/api/generate-code/generateRandomCode.js
@@ -1,5 +1,5 @@
 const crypto = require("crypto");
 
 export default function generateRandomCode() {
-  return crypto.randomBytes(3).toString("hex").toUpperCase();
+  return crypto.randomBytes(6).toString("hex").toUpperCase();
 }
