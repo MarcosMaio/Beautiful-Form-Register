57dccd1fe112195e19e76c8b99a58105a423bb68
Marcos Maio <120725100+MarcosMaio@users.noreply.github.com>
2023-11-15 11:16:42 -0300

    feat: add form-container features to provider more custom items when use as wrapped of forms

--- src/components/form-container.tsx
+++ src/components/form-container.tsx
@@ -2,29 +2,32 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { ReactNode } from "react";
 import styles from "../styles/Form-Container.module.css";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
+import path from "path";
 
 interface Props {
   children: ReactNode;
   height?: string;
   title: string;
   textAlign?: string;
-  fontSize?: string
+  fontSize?: string;
+  path: string;
 }
 export default function FormContainer({
   children,
   height,
   title,
   textAlign,
-  fontSize
+  fontSize,
+  path,
 }: Props) {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper} style={{ height }}>
-        <span className={styles["icon-close"]}>
+        <a className={styles["icon-close"]} href={path}>
           <FontAwesomeIcon width={30} height={30} icon={faXmark} />
-        </span>
+        </a>
         <div className={styles["form-box"]}>
-          <h2 style={{ textAlign , fontSize }} className={styles["input-title"]}>
+          <h2 style={{ textAlign, fontSize }} className={styles["input-title"]}>
             {title}
           </h2>
           {children}
