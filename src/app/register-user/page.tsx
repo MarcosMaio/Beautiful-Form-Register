55d2c52acbc2e07ef6adad736959aac0598d488a
Marcos Maio <120725100+MarcosMaio@users.noreply.github.com>
2023-11-15 11:08:47 -0300

    refactor: change the register page

---
+++ src/app/register-user/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import MessageProviderWrapper from "@/providers/message-provider";
+import RegisterForm from "./components/Register";
+export default function Register() {
+  return (
+      <RegisterForm />
+  );
+}
